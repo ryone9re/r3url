e8f944aaba57d8c81ff54c477cbb86e7e694af15
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-31 20:06:10 +0900

    add kv namepace

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare/globals" />
 /// <reference types="@cloudflare/workers-types" />
+declare const R3URL_KV: KVNamespace;

d8144551904f9ade9b3dab417242c4bdd1ccf0ad
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-01 02:21:15 +0900

    implement darkmode

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from '@remix-run/cloudflare'
+import { createTheme, CssBaseline, useMediaQuery } from '@mui/material'
+import { ThemeProvider } from '@mui/system'
 import {
   Links,
   LiveReload,
@@ -7,6 +8,9 @@ import {
   Scripts,
   ScrollRestoration
 } from '@remix-run/react'
+import { createContext, useState, useMemo } from 'react'
+
+import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,27 +18,63 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1'
 })
 
+export const ColorModeContext = createContext({ toggleColorMode: () => {} })
+
 export default function App() {
+  const prefersColorScheme = useMediaQuery('(prefers-color-scheme: light)')
+  const [mode, setMode] = useState<'light' | 'dark'>(
+    prefersColorScheme ? 'light' : 'dark'
+  )
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+      }
+    }),
+    []
+  )
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: '#455a64'
+          },
+          secondary: {
+            main: '#536dfe'
+          }
+        }
+      }),
+    [mode]
+  )
   return (
-    <html lang='ja'>
-      <head>
-        <Meta />
-        <Links />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-        />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/icon?family=Material+Icons'
-        />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+    <>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <html lang='ja'>
+            <head>
+              <Meta />
+              <Links />
+              <link
+                rel='stylesheet'
+                href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+              />
+              <link
+                rel='stylesheet'
+                href='https://fonts.googleapis.com/icon?family=Material+Icons'
+              />
+            </head>
+            <body>
+              <Outlet />
+              <ScrollRestoration />
+              <Scripts />
+              <LiveReload />
+            </body>
+          </html>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
   )
 }
